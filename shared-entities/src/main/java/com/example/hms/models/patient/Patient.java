bd827857c0a90c76b19970555affe6eb7592cabe
Quan Cao <quancao2310@gmail.com>
2025-03-25 23:59:12 +0700

    chore: add basic initialization for share entities module
    - This is not the final module, because we have decided to split the DB

---
+++ shared-entities/src/main/java/com/example/hms/models/patient/Patient.java
@@ -0,0 +1,24 @@
+package com.example.hms.models.patient;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
