59c7ed1e7f48ac760b2592ab69827ea09a54b9da
Quan Cao <quancao2310@gmail.com>
2025-04-04 23:17:21 +0700

    feat: configure error handling for staff service

---
+++ services/staff-service/src/main/java/com/example/hms/staffservice/common/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.hms.staffservice.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final HttpStatusCode status;
+
+    public CustomException(String message, HttpStatusCode status) {
+        super(message);
+        this.status = status;
+    }
+}
